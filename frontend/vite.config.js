723eb257b4d974fb4ef69acf5e83d335733e0efb
ChrishanAloka <chrishan2002aloka@gmail.com>
2026-02-21 05:27:25 +0530

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -36,7 +36,7 @@ export default defineConfig({
         port: 3000,
         proxy: {
             '/api': {
-                target: 'http://localhost:5000',
+                target: 'https://smartvehiclerepaircenter.onrender.com',
                 changeOrigin: true
             }
         }
